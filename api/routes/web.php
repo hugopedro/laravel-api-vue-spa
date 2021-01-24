dfeeb66e4d1ad2858f5be32a1c5c2e6d7d957451
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-24 18:54:47 -0300

    Configurando e testando conexões na API (backend)

--- api/routes/web.php
+++ api/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test', function () {
+    return response()->json(['msg' => 'Hello World!']);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
