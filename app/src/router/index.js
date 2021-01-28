95078696563c82bacb22edada73dd96ea8643b30
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-28 15:06:22 -0300

    Criando página de registro (frontend)

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
 ];
 
--- app/src/utils/messages.js
+++ app/src/utils/messages.js
@@ -1,4 +1,6 @@
 export default {
   ServerErro: "Ops! Algo deu errado.",
   LoginInvalidException: "Ops! E-mail ou senha inválidos",
+  UserHasBeenTakenException: "Ops! Parece que este usuário já existe.",
+  VerifyEmailTokenInvalidException: "Ops! Parece que este token está inválido.",
 };
