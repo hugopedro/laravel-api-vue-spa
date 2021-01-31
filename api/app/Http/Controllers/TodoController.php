e502ee4608b4164901f1a2b875ad3160c91c14a8
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-31 16:05:20 -0300

    Criando uma todo (backend)

--- api/app/Http/Controllers/TodoController.php
+++ api/app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoStoreRequest;
 use App\Http\Resources\TodoResource;
 
 class TodoController extends Controller
@@ -14,5 +15,13 @@ class TodoController extends Controller
     public function index()
     {
         return TodoResource::collection(auth()->user()->todos);
+    }
+
+    public function store(TodoStoreRequest $request)
+    {
+        $input = $request->validated();
+        $todo = auth()->user()->todos()->create($input);
+
+        return new TodoResource($todo);
     }
 }
