a0a582388eafa37237238676323e2eaa0dbcef1e
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-31 17:22:33 -0300

    Editando todo (backend)

--- api/app/Http/Controllers/TodoController.php
+++ api/app/Http/Controllers/TodoController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoStoreRequest;
+use App\Http\Requests\TodoUpdateRequest;
 use App\Http\Resources\TodoResource;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
@@ -23,5 +25,14 @@ class TodoController extends Controller
         $todo = auth()->user()->todos()->create($input);
 
         return new TodoResource($todo);
+    }
+
+    public function update(Todo $todo, TodoUpdateRequest $request)
+    {
+        $input = $request->validated();
+        $todo->fill($input);
+        $todo->save();
+
+        return new TodoResource($todo->fresh());
     }
 }
